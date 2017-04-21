9bd47961ae60be03305596c163cf47752395bd30
chhe <chhe@ebay.com>
2017-04-21 00:05:58 -0400

    commit

---
+++ Flappy/Assets/Scripts/Bird.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+	public float upForce = 200;
+
+	private bool isDead = false;
+	private Rigidbody2D rb2d;
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isDead == false) {
+			if (Input.GetMouseButtonDown (0)) {
+				rb2d.velocity = Vector2.zero;
+				rb2d.AddForce (new Vector2 (0, upForce));
+				anim.SetTrigger ("Flap");
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(){
+		// Zero out the bird's velocity
+		rb2d.velocity = Vector2.zero;
+		// If the bird collides with something set it to dead...
+		isDead = true;
+		anim.SetTrigger ("Die");
+	}
+}
