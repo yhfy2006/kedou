8189257d98bdd95813bd7ce9e2477c0d9d216ccb
yhfy2006 <ch627@cornell.edu>
2017-05-08 04:15:18 -0400

    Ninja init

---
+++ Ninja/New Unity Project/Assets/Scripts/Camera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x, player.transform.transform.position.y + offset.y, player.transform.transform.position.z + offset.z);
+	}
+}
---
+++ Ninja/New Unity Project/Assets/Scripts/NinjaHero.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NinjaHero : MonoBehaviour {
+    public float upForce = 200;
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.freezeRotation = true;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0)){
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(new Vector2(0, upForce));
+            anim.SetTrigger("Flip");
+        }
+	}
+}
